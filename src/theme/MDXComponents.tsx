77f4bc289285d5f5330a153bd4def0a735d99201
docu.ment <docu.ment@kakaocorp.com>
2024-11-11 18:23:50 +0900

    1

--- src/theme/MDXComponents.tsx
+++ src/theme/MDXComponents.tsx
@@ -2,10 +2,12 @@
 import MDXComponents from "@theme-original/MDXComponents";
 import DocCardList from "@theme/DocCardList";
 import Link from "@docusaurus/Link";
+import ExampleConverter from "@site/src/components/ExampleConverter";
 
 export default {
   ...MDXComponents,
   // 다른 글로벌 컴포넌트를 추가할 수 있습니다.
   DocCardList,
   Link,
+  ExampleConverter,
 };
